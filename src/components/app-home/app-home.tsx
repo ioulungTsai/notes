d4801f8f59acda2a82ba0c7f6c33ca8205c149b8
brandonTsai <brandonya125@gmail.com>
2022-10-16 03:19:21 -0700

    Connect app-home with fsk-notes-list

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -9,7 +9,7 @@ export class AppHome {
   render() {
     return (
       <div class="app-home">
-        TODO: call notes-list component
+        <fsk-notes-list></fsk-notes-list>
       </div>
     );
   }
