941fce1aee31b390df1b0b14a5dff57910a0f7d2
brandonTsai <brandonya125@gmail.com>
2022-10-15 22:02:42 -0700

    Generate notes-list component

---
+++ src/components/fsk-notes-list/fsk-notes-list.tsx
@@ -0,0 +1,18 @@
+import { Component, Host, h } from '@stencil/core';
+
+@Component({
+  tag: 'fsk-notes-list',
+  styleUrl: 'fsk-notes-list.css',
+  shadow: true,
+})
+export class FskNotesList {
+
+  render() {
+    return (
+      <div>
+        TODO: Create notes-list render
+      </div>
+    );
+  }
+
+}
