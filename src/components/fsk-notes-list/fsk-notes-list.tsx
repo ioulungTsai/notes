0fa22e750f2bb71e0e73f60bcf9784025a42b194
brandonTsai <brandonya125@gmail.com>
2022-10-16 02:45:26 -0700

    Added documentation

--- src/components/fsk-notes-list/fsk-notes-list.tsx
+++ src/components/fsk-notes-list/fsk-notes-list.tsx
@@ -1,5 +1,8 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, h } from '@stencil/core';
 
+/**
+ * Lists notes
+ */
 @Component({
   tag: 'fsk-notes-list',
   styleUrl: 'fsk-notes-list.css',
