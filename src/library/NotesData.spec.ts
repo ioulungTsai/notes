5fc56dbfdd4a66f4314831e74abaa7f358470447
brandonTsai <brandonya125@gmail.com>
2022-10-17 01:55:14 -0700

    Create note text in NotesData

--- src/library/NotesData.spec.ts
+++ src/library/NotesData.spec.ts
@@ -3,10 +3,10 @@ import * as noteData from './NotesData'
 describe('NotesData Tests', () => {
   const expectedData = JSON.parse(
   `[
-    {"id":"1","datetime":"2022-10-16T10:10Z","title":"1st Note"},
-    {"id":"2","datetime":"2022-10-17T10:11Z","title":"2nd Note"},
-    {"id":"3","datetime":"2022-10-18T10:12Z","title":"3rd Note"},
-    {"id":"4","datetime":"2022-10-19T10:13Z","title":"4th Note"}
+    {"id":"1","datetime":"2022-10-16T10:10Z","title":"My 1st Note"},
+    {"id":"2","datetime":"2022-10-17T10:11Z","title":"My 2nd Note"},
+    {"id":"3","datetime":"2022-10-18T10:12Z","title":"My 3rd Note"},
+    {"id":"4","datetime":"2022-10-19T10:13Z","title":"My 4th Note"}
   ]`)
 
   test('getList returns expected data',async () => {
@@ -15,7 +15,7 @@ describe('NotesData Tests', () => {
 
   test('getNote returns expected note', () => {
     const expectedResults = JSON.parse(`
-      {"id":"1","datetime":"2022-10-16T10:10Z","title":"1st Note"}
+      {"id":"1","datetime":"2022-10-16T10:10Z","title":"My 1st Note","text": "Text for my 1st Note"}
     `)
     expect(noteData.getNote(1)).toEqual(expectedResults)
   })
--- src/library/NotesData.ts
+++ src/library/NotesData.ts
@@ -5,10 +5,18 @@
 
 const list = JSON.parse(
 `[
-  {"id":"1","datetime":"2022-10-16T10:10Z","title":"1st Note"},
-  {"id":"2","datetime":"2022-10-17T10:11Z","title":"2nd Note"},
-  {"id":"3","datetime":"2022-10-18T10:12Z","title":"3rd Note"},
-  {"id":"4","datetime":"2022-10-19T10:13Z","title":"4th Note"}
+  {"id":"1","datetime":"2022-10-16T10:10Z","title":"My 1st Note"},
+  {"id":"2","datetime":"2022-10-17T10:11Z","title":"My 2nd Note"},
+  {"id":"3","datetime":"2022-10-18T10:12Z","title":"My 3rd Note"},
+  {"id":"4","datetime":"2022-10-19T10:13Z","title":"My 4th Note"}
+]`)
+
+const text = JSON.parse(
+`[
+  {"id":"1","text":"Text for my 1st Note"},
+  {"id":"2","text":"Text for my 2nd Note"},
+  {"id":"3","text":"Text for my 3rd Note"},
+  {"id":"4","text":"Text for my 4th Note"}
 ]`)
 
 /**
@@ -23,5 +31,8 @@ export function getList() {
  * @param id : Id of the note to fetch
  */
 export function getNote(id: number) {
-  return(list[id - 1])
+  const note = list[id-1]
+  const clonedNote = {...note}
+  clonedNote.text = text[id-1].text
+  return(clonedNote)
 }
