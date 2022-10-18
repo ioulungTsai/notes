f002ed40941d55ccc09940789f1ec8d784c148c2
brandonTsai <brandonya125@gmail.com>
2022-10-18 15:06:46 -0700

    Implement black box testing for app.ts

---
+++ server/gateway/src/app.ts
@@ -0,0 +1,10 @@
+/**
+ * Main application router for gateway
+ */
+import express from 'express'
+export const app = express();
+
+app.get('/', (req, res) => {
+  console.log('get /');
+  res.send('Hello from Express!');
+});
--- server/gateway/src/index.ts
+++ server/gateway/src/index.ts
@@ -1,13 +1,7 @@
 /**
  * Main application router for gateway
  */
-import express from 'express'
-const app = express();
-
-app.get('/', (req, res) => {
-  console.log('get /');
-  res.send('<h1>Hello from Express!</h1>');
-});
+import { app } from "./app"
 
 const port = process.env.port || 8080;
 
