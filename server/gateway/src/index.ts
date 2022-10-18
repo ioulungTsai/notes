31163ee1ae9352a51be91c157db32ae8b9d8c7d3
brandonTsai <brandonya125@gmail.com>
2022-10-18 16:31:24 -0700

    Use ctrl-c to out of container

--- server/gateway/src/index.ts
+++ server/gateway/src/index.ts
@@ -8,3 +8,7 @@ const port = process.env.port || 8080;
 app.listen(port, () => {
   console.log(`server up and listening on the port ${port}!`);
 });
+
+process.on("SIGINT", function() {
+  process.exit()
+})
