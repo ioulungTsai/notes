f0a8c008a52d58308c625c959a6cd4641b407f02
brandonTsai <brandonya125@gmail.com>
2022-10-18 17:36:45 -0700

    Fix CORS access

--- server/gateway/src/app.ts
+++ server/gateway/src/app.ts
@@ -2,7 +2,11 @@
  * Main application router for gateway
  */
 import express from 'express'
+import cors from 'cors'
 export const app = express();
+
+app.use(cors())
+app.options('*', cors())
 
 app.get('/', (req, res) => {
   console.log('get /');
