cdbe9e5f5b3d35bd930f6bc0eff243f2e145deda
brandonTsai <brandonya125@gmail.com>
2022-10-20 14:30:40 -0700

    Update NotesData unit tests

--- src/library/NotesData.spec.ts
+++ src/library/NotesData.spec.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const mock = jest.spyOn(axios, 'get')
+const getMock = jest.spyOn(axios, 'get')
 
 import * as notesData from './NotesData'
 
@@ -13,18 +13,21 @@ describe('NotesData Tests', () => {
   ]`)
 
   test('getList returns expected data',async () => {
-    mock.mockResolvedValue({data: expectedData})
+    getMock.mockResolvedValue({data: expectedData})
     const data = await notesData.getList()
     expect(data).toEqual(expectedData)
+    expect(getMock).toHaveBeenCalledWith('/api/list')
   })
 
   test('getNote returns expected note', async () => {
     const expectedResults = JSON.parse(`
       {"id":"1","datetime":"2022-10-16T10:10Z","title":"My 1st Note","text": "Text for my 1st Note"}
     `)
-    mock.mockResolvedValue({data: expectedResults})
+    getMock.mockClear()
+    getMock.mockResolvedValue({data: expectedResults})
     const note = await notesData.getNote(1)
     expect(note).toEqual(expectedResults)
+    expect(getMock).toHaveBeenCalledWith('/api/note/1')
   })
 
   test('saveNote should save a note', async () => {
@@ -33,6 +36,11 @@ describe('NotesData Tests', () => {
 
     const saveResult = await notesData.saveNote(1, 'Edited Test Title', 'Edited Test Text')
     expect(saveResult).toBe(1)
+    expect(putMock).toHaveBeenCalledWith(
+      '/api/note/save/1',
+      {title: 'Edited Test Title', text: 'Edited Test Text'},
+      {'headers': {'Content-Type':'application/json'}}
+    )
   })
 
   test('addNote should add a new note', async () => {
@@ -43,6 +51,7 @@ describe('NotesData Tests', () => {
     // Add note 5 check for results
     const newNodeId = await notesData.addNote()
     expect(newNodeId).toBe(5)
+    expect(postMock).toHaveBeenCalledWith('/api/note/add')
   })
 
   test('deleteNote deletes the right note', async () => {
@@ -50,5 +59,6 @@ describe('NotesData Tests', () => {
     deleteMock.mockResolvedValue({data: '2'})
     const noteId = await notesData.deleteNote(2)
     expect(noteId).toBe(2)
+    expect(deleteMock).toHaveBeenCalledWith('/api/note/2')
   })
 })
