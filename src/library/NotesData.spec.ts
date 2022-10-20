37a94aa4e0975d36d6da4ac32eeafac87c7ec0f3
brandonTsai <brandonya125@gmail.com>
2022-10-19 23:47:11 -0700

    Update getNote, addNote & deleteNote in the client

--- src/library/NotesData.spec.ts
+++ src/library/NotesData.spec.ts
@@ -22,15 +22,9 @@ describe('NotesData Tests', () => {
     const expectedResults = JSON.parse(`
       {"id":"1","datetime":"2022-10-16T10:10Z","title":"My 1st Note","text": "Text for my 1st Note"}
     `)
-    mock.mockResolvedValue({data: 'note'})
+    mock.mockResolvedValue({data: expectedResults})
     const note = await notesData.getNote(1)
     expect(note).toEqual(expectedResults)
-  })
-
-  test('getNote returns empty object if id is invalid', async () => {
-    mock.mockResolvedValue({data: 'invalid id'})
-    const note = await notesData.getNote(-1)
-    expect(Object.keys(note).length).toBe(0)
   })
 
   test('saveNote should save a note', async () => {
@@ -42,47 +36,25 @@ describe('NotesData Tests', () => {
 
     notesData.saveNote(1, 'Edited Test Title', 'Edited Test Text')
 
-    mock.mockResolvedValue({data: 'saved note'})
+    mock.mockResolvedValue({data: expectedResults})
     const note = await notesData.getNote(1)
     expect(note).toEqual(expectedResults)
   })
 
   test('addNote should add a new note', async () => {
-    const expectedResults = JSON.parse(`
-      {
-        "id":"5",
-        "datetime":"2022-11-22T11:22:00.000Z",
-        "title":"untitled",
-        "text":""
-      }
-    `)
-
-    // Mock Date.now() to return a fixed testable date-time
-    jest.spyOn(global.Date, 'now')
-    .mockImplementationOnce(() =>
-      new Date('2022-11-22T11:22Z').valueOf()
-    )
-
     // Mock axios.post
     const postMock = jest.spyOn(axios, 'post')
-    postMock.mockResolvedValue({data: 'add note'})
+    postMock.mockResolvedValue({data: '5'})
 
     // Add note 5 check for results
     const newNodeId = await notesData.addNote()
     expect(newNodeId).toBe(5)
-
-    mock.mockResolvedValue({data: 'added note'})
-    const note = await notesData.getNote(newNodeId)
-    expect(note).toEqual(expectedResults)
   })
 
   test('deleteNote deletes the right note', async () => {
     const deleteMock = jest.spyOn(axios, 'delete')
-    deleteMock.mockResolvedValue({data: 'delete note'})
-    await notesData.deleteNote(2)
-
-    mock.mockResolvedValue({data: 'deleted note'})
-    const note = await notesData.getNote(2)
-    expect(Object.keys(note).length).toBe(0)
+    deleteMock.mockResolvedValue({data: '2'})
+    const noteId = await notesData.deleteNote(2)
+    expect(noteId).toBe(2)
   })
 })
