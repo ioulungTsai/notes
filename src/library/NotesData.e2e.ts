a73595614e24c53da8df73856de81e369b531ec5
brandonTsai <brandonya125@gmail.com>
2022-10-22 12:49:22 -0700

    Update NotesData for DB connecting

--- src/library/NotesData.e2e.ts
+++ src/library/NotesData.e2e.ts
@@ -7,10 +7,10 @@ import * as notesData from './NotesData'
 describe('NotesData Integration Tests', () => {
   const expectedData = JSON.parse(
     `[
-      {"id":"1","datetime":"2022-10-16T10:10Z","title":"My 1st Note"},
-      {"id":"2","datetime":"2022-10-17T10:11Z","title":"My 2nd Note"},
-      {"id":"3","datetime":"2022-10-18T10:12Z","title":"My 3rd Note"},
-      {"id":"4","datetime":"2022-10-19T10:13Z","title":"My 4th Note"}
+      {"title":"My 1st Note"},
+      {"title":"My 2nd Note"},
+      {"title":"My 3rd Note"},
+      {"title":"My 4th Note"}
     ]`)
 
   beforeEach(async () => {
@@ -22,22 +22,31 @@ describe('NotesData Integration Tests', () => {
   })
 
   it('should get a Notes List', async () => {
-    const data = await notesData.getList()
-    expect(data).toEqual(expectedData)
+    const list = await notesData.getList()
+    expect(list.length).toBe(4)
+    for(let i = 0; i < list.length; ++i)
+      expect(list[i].title).toEqual(expectedData[i].title)
   })
 
   it('should get a Note or get an error', async () => {
     const expectedResults = JSON.parse(`
-      {"id":"1","datetime":"2022-10-16T10:10Z","title":"My 1st Note","text": "Text for my 1st Note"}
+      {"title":"My 1st Note","text": "Text for my 1st Note"}
     `)
 
+    // Get note id from the notes list
+    const list = await notesData.getList()
+    const noteId = list[0].id
+
     // Test we get a note as expected
-    const data = await notesData.getNote(1)
-    expect(data).toEqual(expectedResults)
+    const note = await notesData.getNote(noteId)
+    expect(note.id).toBe(noteId)
+    expect(note.datetime).toBe(list[0].datetime)
+    expect(note.title).toBe(expectedResults.title)
+    expect(note.text).toBe(expectedResults.text)
 
     // Test that a bad id returns an error from the server
     try {
-      await notesData.getNote(-1)
+      await notesData.getNote('-1')
       fail('getNote did not rturn an error')
     } catch (error) {
       expect(error.response.status).toBe(404)
@@ -48,23 +57,26 @@ describe('NotesData Integration Tests', () => {
   it('should add a note', async () => {
     // Create note and check id
     const noteId = await notesData.addNote()
-    expect(noteId).toBe(5)
 
     // Fetch note to make sure it was created
-    const note = await notesData.getNote(5)
-    expect(note.id).toBe(noteId.toString())
+    const note = await notesData.getNote(noteId)
+    expect(note.id).toBe(noteId)
     expect(note.text).toBe('')
     expect(note.title).toBe('untitled')
   })
 
   it('should delete a note or get an error', async () => {
+    // Get note id from the notes list
+    const list = await notesData.getList()
+    const noteId = list[0].id
+
     // delete note and check returned id
-    const noteId = await notesData.deleteNote(2)
-    expect(noteId).toBe(2)
+    const deletedId = await notesData.deleteNote(noteId)
+    expect(deletedId).toBe(noteId)
 
     // fetch note #2 to prove it does note exist
     try {
-      await notesData.getNote(2)
+      await notesData.getNote("2")
       fail('getNote did not rturn an error')
     } catch (error) {
       expect(error.response.status).toBe(404)
@@ -73,7 +85,7 @@ describe('NotesData Integration Tests', () => {
 
     // delete note that does not exist
     try {
-      await notesData.deleteNote(2)
+      await notesData.deleteNote('2')
       fail('deleteNote did not rturn an error')
     } catch (error) {
       expect(error.response.status).toBe(404)
@@ -82,17 +94,22 @@ describe('NotesData Integration Tests', () => {
   })
 
   it('should save a note or get an error', async () => {
-    const noteId = await notesData.saveNote(1, 'Edited Test Title', 'Edited Test Text')
-    expect(noteId).toBe(1)
+    // Get note id from the notes list
+    const list = await notesData.getList()
+    const noteId = list[0].id
 
-    // Test we get a note as expected
-    const note = await notesData.getNote(1)
+    // save note and check returned id
+    const savedId = await notesData.saveNote(noteId, 'Edited Test Title', 'Edited Test Text')
+    expect(savedId).toBe(noteId)
+
+    // fetch saved note and check its content
+    const note = await notesData.getNote(noteId)
     expect(note.title).toBe('Edited Test Title')
     expect(note.text).toBe('Edited Test Text')
 
     // Test that a bad id returns an error from the server
     try {
-      await notesData.saveNote(-1, 'Edited Test Title', 'Edited Test Text')
+      await notesData.saveNote('-1', 'Edited Test Title', 'Edited Test Text')
       fail('saveNote did not rturn an error')
     } catch (error) {
       expect(error.response.status).toBe(404)
