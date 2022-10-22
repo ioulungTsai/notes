b50cc57f252086cd902864cdd74c01365153bad4
brandonTsai <brandonya125@gmail.com>
2022-10-22 00:44:58 -0700

    Connect to database from server

--- server/gateway/jest.config.js
+++ server/gateway/jest.config.js
@@ -1,8 +1,13 @@
 /* eslint-env node */
+process.env.dbUsername = 'HDB_ADMIN';
+process.env.dbPassword = 'password';
+process.env.dbUrl = 'http://localhost:9925';
+
 module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  testEnvironment: 'node',
 };
--- server/gateway/src/Utils.spec.ts
+++ server/gateway/src/Utils.spec.ts
@@ -29,4 +29,11 @@ describe('Utils tests', () => {
       `)
     )
   })
+
+  test('DBConnect gets query response from database', async () => {
+    const db = new Utils.DBconnect()
+    const query = {"operation":"system_information"}
+    const response = await db.send(query)
+    expect(JSON.stringify(response)).toMatch('"platform":"linux"')
+  })
 })
